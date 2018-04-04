c862e1171b43f95007cd09cb6e08c36ce37604fd
bpham <brianthongpham@gmail.com>
2018-04-03 21:26:09 -0700

    put in a random article

---
+++ Assignment-3-Parents_N_Child/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+
+using namespace std;
+
+int main(){
+
+    string randomArticlePart = "Lochry's Defeat, also known as the Lochry massacre, was a battle fought on 
+    August 24, 1781, near present-day Aurora, Indiana, in the United States. The battle was part of the 
+    American Revolutionary War (1775–1783), which began as a conflict between Great Britain and the Thirteen 
+    Colonies before spreading to the western frontier, where American Indians entered the war as British 
+    allies. The battle was short and decisive: about one hundred Indians of local tribes led by Joseph Brant, 
+    a Mohawk military leader who was temporarily in the west, ambushed a similar number of Pennsylvania 
+    militiamen led by Archibald Lochry. Brant and his men killed or captured all of the Pennsylvanians 
+    without suffering any casualties.
+    
+    Lochry's force was part of an army being raised by George Rogers Clark for a campaign against Detroit, 
+    the British regional headquarters. Clark, the preeminent American military leader on the northwestern 
+    frontier, worked with Governor Thomas Jefferson of Virginia in planning an expedition to capture Detroit, 
+    by which they hoped to bring an end to British support of the Indian war effort. In early August 1781, 
+    Clark and about 400 men left Fort Pitt in Pennsylvania by boat, floating down the Ohio River a few days 
+    ahead of Lochry and his men, who were trying to catch up.
+    
+    Joseph Brant's force was part of a combined British and Indian army being raised to counter Clark's 
+    offensive. Brant had too few men to challenge Clark, but when he intercepted messengers traveling between 
+    Clark and Lochry, he learned about Lochry's smaller group bringing up the rear. When Lochry landed to 
+    feed his men and horses, Brant launched his overwhelmingly successful ambush. Because Clark had been 
+    able to recruit only a fraction of the men he needed for his campaign, the loss of Lochry's men resulted 
+    in the cancellation of Clark's expedition.";
+
+    cout << "====================Parents and Child Processes a CECS 326 Project===============================" << endl;
+    cout << "=================================================================================================" << endl;
+    cout << "==================================A random article===============================================" << endl;
+    cout << randomArticlePart << endl;
+
+    return 0;
+}
