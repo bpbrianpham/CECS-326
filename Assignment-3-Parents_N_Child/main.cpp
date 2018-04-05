2cc860799f11f6de169f25c810be52896c97f5fe
bpham <brianthongpham@gmail.com>
2018-04-04 19:50:09 -0700

    utilize reading/writing from text file method

--- Assignment-3-Parents_N_Child/main.cpp
+++ Assignment-3-Parents_N_Child/main.cpp
@@ -1,41 +1,33 @@
 #include <iostream>
+#include <unistd.h>
 #include <cstdlib>
 #include <time.h>
+#include <fstream>
 
 using namespace std;
-
-
 
 int main(){
 
     cout << "\n====================Parents and Child Processes a CECS 326 Project===============================\n" << endl;
     cout << "=================================================================================================\n" << endl;
     cout << "==================================A random article===============================================\n" << endl;
-    cout << "Android is a mobile operating system developed by Google, based on a modified version of the \n" 
-    << "Linux kernel and other open source software and designed primarily for touchscreen mobile devices \n"
-    << "such as smartphones and tablets. In addition, Google has further developed Android TV for televisions, \n"
-    << "Android Auto for cars, and Wear OS for wrist watches, each with a specialized user interface. \n"
-    << "Variants of Android are also used on game consoles, digital cameras, PCs and other electronics.\n"
-    << "Initially developed by Android Inc., which Google bought in 2005, Android was unveiled in 2007, \n"
-    << "with the first commercial Android device launched in September 2008. The operating system has \n"
-    << "since gone through multiple major releases, with the current version being 8.1 Oreo, released \n"
-    << "in December 2017. The core Android source code is known as Android Open Source Project (AOSP), \n"
-    << "and is primarily licensed under the Apache License.\n\n"
-    << "Android is also associated with a suite of proprietary software developed by Google, including \n"
-    << "core apps for services such as Gmail and Google Search, as well as the application store and \n"
-    << "digital distribution platform Google Play, and associated development platform. These apps are \n"
-    << "licensed by manufacturers of Android devices certified under standards imposed by Google, but AOSP\n" 
-    << "has been used as the basis of competing Android ecosystems, such as Amazon.com's Fire OS, which \n"
-    << "utilize its own equivalents to these Google Mobile Services.\n\n"
-    << "Android has been the best-selling OS worldwide on smartphones since 2011 and on tablets since 2013. \n"
-    << "As of May 2017, it has over two billion monthly active users, the largest installed base of any \n"
-    << "operating system, and as of 2017, the Google Play store features over 3.5 million apps."
-    << endl;
+    
+    ifstream inFile;
+    string line;
+    inFile.open("../randomArticle.txt");
+
+    if(inFile.is_open()){
+        while(getline(inFile, line)){
+            cout << line << endl;
+        }
+        inFile.close();
+    }
+    else{
+        cout << "Unable to open file" << endl;
+    }
 
 
-    string target = "";
-    cout << "\n\nEnter targetted word: ";
-    cin >> target;
+    
 
     return 0;
 }
