3fbc14679645992aca6a96d7f4f17ba9da606b64
bpham <brianthongpham@gmail.com>
2018-05-08 21:22:33 -0700

    test

--- Assignment-4-Semaphores_N_Shared_Memory/main.cpp
+++ Assignment-4-Semaphores_N_Shared_Memory/main.cpp
@@ -49,15 +49,19 @@ int main(){
 					arr[3] = child;
 				}else{//child D
 					while(true){calculate(sem, shmBUF, "D");}
+					cout << "D is done." << endl;
 				}
 			}else{//child C
 				while(true){calculate(sem, shmBUF, "C");}
+				cout << "C is done." << endl;
 			}
 		}else{//child B
 			while(true){calculate(sem, shmBUF, "B");}
+			cout << "B is done." << endl;
 		}
 	}else{//child A
 		while(true){calculate(sem, shmBUF, "A");}
+		cout << "A is done." << endl;
 	}
 
 
@@ -79,7 +83,7 @@ void calculate(SEMAPHORE &sem, bool *shmBUF, string childLetter) {
 			randNum = rand() % 100000;
 			cout << childLetter << " : " << randNum << " compare to " << V << endl;
 		}while(randNum % V == 0 || randNum < 100);
-		cout << childLetter << "is done.";
+		
 		*shmBUF = true;
 	}else{
 		int U = 827395609;
@@ -88,7 +92,7 @@ void calculate(SEMAPHORE &sem, bool *shmBUF, string childLetter) {
 			randNum = rand() % 100000;
 			cout << childLetter << " : " << randNum << " compare to " << U << endl;
 		}while(randNum % U == 0 || randNum < 100);
-		cout << childLetter << "is done.";
+		
 	}
 	
 	sem.P(UVsemaphore);
